0c45781f0a152f8dd60b890fca10fc307f8c6008
Aimal <aimalhassan3333@gmail.com>
2026-05-14 23:17:58 +0500

    Added backend server

---
+++ team-task-manager/server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+
+  res.send("Backend running successfully");
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on ${PORT}`);
+
+});
